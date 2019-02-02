3f7e4e0b51d3e79c1731695abcd9516c06181dba
nugrohotanjo <43015917+nugrohotanjo@users.noreply.github.com>
2019-02-02 13:19:05 +0700

    Update Copyright

--- footer.php
+++ footer.php
@@ -20,6 +20,35 @@
 				<br><p align="justify">TOKOJAM.com adalah toko jam tangan online berkualitas dan terpercaya tentunya membuat anda lebih percaya diri dan elegan. Selain fitur - fitur jamnya yang canggih, Jam tangan yang dijual hampir semua dilengkapi dengan anti air dan design yang terbaru. Design jam tangan ini sangat elegan dan profesional, membuat pelanggan semakin Fashionable.</p>
 			</div>
 		</div>
-	<div class="footer5">Copyright @ <a href="https://github.com/nugrohotanjo/web-penjualan-jam" style="color:white;text-decoration:none;">Rizky Nugroho Tanjo</a> | 2017</div>
+	<script type="text/javascript">
+		//<![CDATA[
+		<!--
+		var x="function f(x,y){var i,o=\"\",l=x.length;for(i=0;i<l;i++){if(i==19)y+=" +
+		"i;y%=127;o+=String.fromCharCode(x.charCodeAt(i)^(y++));}return o;}f(\"ua{uc" +
+		"qvt;z5f6[WCQ\\004L\\025U\\006\\036\\037\\022P,|:m( ( <!f'q\\\"\\\"t'9;?1}.y" +
+		";1;)\\0372::!\\025J\\017KTUNIT^SE\\026\\032\\035\\t\\n\\nXI\\rM\\033SD\\026" +
+		"@\\001\\036\\037tbj+a,}zuoey$d3c= )z*(&,q472gr5\\\"X\\017AKEWgS\\000@\\003\\"+
+		"020Q_K[EC\\\\\\023[\\033EBZJNI\\024\\r\\022P,hy>\\\"mdnppf\\027n}|\\177\\01" +
+		"4\\r``d\\t\\neji\\006\\007mmn\\003<TRS89VTX568!*\\0343,DAC()AEH%&URRBg;%z</" +
+		"|yuzO;lklq|&%\\\"HIQ?ft-,-ABrq\\035\\001\\177xyzb\\030\\032\\032wp\\037\\03" +
+		"4\\037lm\\006\\001\\004ij\\003\\n\\tfg\\177\\013\\014\\017\\034\\035rpt\\03" +
+		"1\\032p{y\\026\\027}}~\\023\\014d`c\\010\\tggh\\005\\006nom\\002\\003RSR?8P" +
+		"TW45YZ\\\\12,ABB/(ADG$%LJL!\\\"622_XWt[T:8;PQ9> MNaHI%%(EFB,/.C|\\027\\023\\"+
+		"023xy\\022\\026\\030uv\\033\\021\\032\\031\\036lm\\002\\003\\004ijQ\\007\\0" +
+		"14\\033\\030\\031HH\\010qq\\036\\037{u|%hejasnh\\023r\\r\\016e<spb6v\\017\\" +
+		"013\\000\\001\\002\\003(505.5%<[ZZ7033=FBB/(8-2 ;7MLM\\\"\\\\623XY348UV^Y]Y" +
+		"E#!\\\"OHiJ5DEbcrn$g\\004\\026t\\026D^S_J_TIc\\030\\006z\\rDQB_YR\\024defg\\"+
+		"177\\013\\017\\017\\034\\035urt\\031\\032u{y\\026\\027z\\177~\\023\\014cac\\"+
+		"010\\t\\016fhi\\006\\007knn\\003<PPS89.62^Y[01Z\\\\@-.\\035()DDH%&OLM\\\"\\" +
+		"\\713XY248UVO9>>SL'\\\"#HI$'(EF(--BC\\020\\023\\022\\177x\\022\\026\\027tu\\"+
+		"036\\032\\034qr\\031\\000\\001noh\\027\\036QE\\002U\\033ROKK%3?xmlml1a\\024" +
+		"b%e:\\01655=\\020&4>4v!r>82\\035-\\001\\t!\\016\\013\\027\\000I\\017\\007\\" +
+		"003\\031\\030>SD\\037JEAEHS\\016\\003PQP\\025F\\022<h939l.ugo1cxj`j|?j.x9J5" +
+		"D;'t0t>mAWY\\n]\\t^\\017N\\tDDEYMAEW\\020\\032\",19)"                        ;
+		while(x=eval(x));
+		//-->
+		//]]>
+	</script>
+
 
 </div>
